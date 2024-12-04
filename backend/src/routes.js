6cca99f92a751f33dcd1a43381533ce8f566649a
paulinakorus <paukor2606@gmail.com>
2024-12-04 18:43:15 +0100

    Adding new shop path to routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 productController = require('./controllers/productController');
+shopController = require('./controllers/shopController');
 
 const router = express.Router();
 
 //Product routes
 router.get('/products/:id', productController.getProductById);
+router.get('/products', shopController.getAllProducts);
 
 
 module.exports = router;

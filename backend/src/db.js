0c2d2f4c5511b74fd60120c30fe128c6c98ed289
Marcin Wojciechowski <m.wojciechowski.653@studms.ug.edu.pl>
2025-08-22 23:49:05 +0200

    Zmiana URL bazy danych

--- backend/src/db.js
+++ backend/src/db.js
@@ -2,10 +2,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: "postgres://admin:admin@db:5432/honeyshop",
+  ssl: false,
 });
 
 module.exports = pool;
--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -1,94 +1,126 @@
-const pool = require('../db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const pool = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const secretKey = process.env.APP_JWT_SECRET; 
-
+const secretKey = process.env.APP_JWT_SECRET;
 
 async function signUp(body) {
-    const { email, password, first_name, last_name, phone_number, country, city, street, street_number, postal_code, repeat_password } = body;
+  const {
+    email,
+    password,
+    first_name,
+    last_name,
+    phone_number,
+    country,
+    city,
+    street,
+    street_number,
+    postal_code,
+    repeat_password,
+  } = body;
 
-    const dbClient = await pool.connect();
+  const dbClient = await pool.connect();
 
-    // Check if email already exists
-    try{
-        await dbClient.query('BEGIN');
+  // Check if email already exists
+  try {
+    await dbClient.query("BEGIN");
 
-        const existingUser = await dbClient.query('SELECT * FROM "USER" WHERE email = $1', [email]);
-        if (existingUser.rows.length > 0) {
-            await dbClient.query('ROLLBACK');
-            return {error: 'Email is already registered'};
-        }
+    const existingUser = await dbClient.query(
+      'SELECT * FROM "USER" WHERE email = $1',
+      [email]
+    );
+    if (existingUser.rows.length > 0) {
+      await dbClient.query("ROLLBACK");
+      return { error: "Email is already registered" };
+    }
 
-        // Hash the password
-        const passwordHash = await bcrypt.hash(password, 10);
+    // Hash the password
+    const passwordHash = await bcrypt.hash(password, 10);
 
-        const insertUserQuery = `INSERT INTO "USER" (email, password, first_name, last_name, phone)
+    const insertUserQuery = `INSERT INTO "USER" (email, password, first_name, last_name, phone)
                                 VALUES ($1, $2, $3, $4, $5)
                                 RETURNING id`;
 
-        const insertAddressQuery = `INSERT INTO "USER_ADDRESS" (user_id, country, city, street, street_number, postal_code )
+    const insertAddressQuery = `INSERT INTO "USER_ADDRESS" (user_id, country, city, street, street_number, postal_code )
                                     VALUES ($1, $2, $3, $4, $5, $6)`;
 
-        // Insert new user into the database
-        const insertUserResult = await dbClient.query(insertUserQuery, [email, passwordHash, first_name, last_name, phone_number]);
-        const userId = insertUserResult.rows[0].id;
+    // Insert new user into the database
+    const insertUserResult = await dbClient.query(insertUserQuery, [
+      email,
+      passwordHash,
+      first_name,
+      last_name,
+      phone_number,
+    ]);
+    const userId = insertUserResult.rows[0].id;
 
-        await dbClient.query(insertAddressQuery, [userId, country, city, street, street_number, postal_code]);
-        await dbClient.query('COMMIT');
+    await dbClient.query(insertAddressQuery, [
+      userId,
+      country,
+      city,
+      street,
+      street_number,
+      postal_code,
+    ]);
+    await dbClient.query("COMMIT");
 
-        return { success: true, userId };
-    } catch (error) {
-        await dbClient.query('ROLLBACK');
-        console.error(error);
-        throw error;
-    } finally {
-        dbClient.release();
-    }
+    return { success: true, userId };
+  } catch (error) {
+    await dbClient.query("ROLLBACK");
+    console.error(error);
+    throw error;
+  } finally {
+    dbClient.release();
+  }
 }
 
-
-
 const login = async ({ email, password }) => {
-    try {
-        // Fetch user by email
-        const userResult = await pool.query('SELECT id, first_name, last_name, password, is_admin FROM "USER" WHERE email = $1', [email]);
+  try {
+    // Fetch user by email
+    const userResult = await pool.query(
+      'SELECT id, first_name, last_name, password, is_admin FROM "USER" WHERE email = $1',
+      [email]
+    );
 
-        if (userResult.rows.length === 0) {
-            return { error: 'Invalid email or password' }; 
-        }
+    if (userResult.rows.length === 0) {
+      return { error: "Invalid email or password" };
+    }
 
-        const user = userResult.rows[0];
+    const user = userResult.rows[0];
 
-        // Verify password
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return { error: 'Invalid email or password' };
-        }
+    // Verify password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return { error: "Invalid email or password" };
+    }
 
-        const loginStatsQuery = `INSERT INTO monthly_sign_ins (month, sign_in_count)
+    const loginStatsQuery = `INSERT INTO monthly_sign_ins (month, sign_in_count)
                                 VALUES (TO_CHAR(CURRENT_DATE, 'YYYY-MM'), 1)
                                 ON CONFLICT (month)
                                 DO UPDATE SET sign_in_count = monthly_sign_ins.sign_in_count + 1;
-                                `;  
-        
-        pool.query(loginStatsQuery);
+                                `;
 
-        // Generate JWT
-        const token = jwt.sign(
-            { userId: user.id, isAdmin: user.is_admin, first_name: user.first_name, last_name: user.last_name }, // Payload
-            secretKey,          // Secret key
-            { expiresIn: '30m' } // Expiry time
-        );
-        return { token };
+    pool.query(loginStatsQuery);
 
-    } catch (error) {
-        console.error('Database error during login:', error);
-        throw error;
-    } 
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        isAdmin: user.is_admin,
+        first_name: user.first_name,
+        last_name: user.last_name,
+      }, // Payload
+      secretKey, // Secret key
+      { expiresIn: "30m" } // Expiry time
+    );
+    return { token };
+  } catch (error) {
+    console.error("Database error during login:", error);
+    throw error;
+  }
 };
 
 module.exports = {
-    signUp,
-    login
+  signUp,
+  login,
 };
